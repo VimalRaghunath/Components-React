fb1ee00933ee81dadf31ef3f634e1c95e1cc1639
VimalRaghunath <vimalvrk@gmail.com>
2023-07-11 10:32:25 +0530

    first commit

---
+++ src/components/comp1.js
@@ -0,0 +1,32 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+import React from "react";
+
+function Contact (){
+  return(
+      
+    <div>
+          <h1>Login To Our Store</h1>
+          <label>Email/Username</label>
+          <input type="text"/>
+          <br/>
+          <label>Password</label>
+          <input type="password"/>
+          <br/>
+          <button>----------LOGIN NOW----------</button>
+
+
+    </div>
+
+  )
+}
+export default Contact;
+
+
+
+
+
+
+
+
+
+ 
---
+++ src/components/comp2.jsx
@@ -0,0 +1,10 @@
+function Forgetpassword(){
+    return (
+   <div>
+      <h4>Forget Password?<button><b>Click Here</b></button></h4>
+   </div>
+
+    )
+}
+
+export default Forgetpassword;
